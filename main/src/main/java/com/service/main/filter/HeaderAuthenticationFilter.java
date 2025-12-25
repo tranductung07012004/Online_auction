995a77790dec0137ce0645ee7854ba206526ba47
TranDucTung <tranductung07012004@gmail.com>
2025-12-25 21:11:20 +0700

    small change

--- main/src/main/java/com/service/main/filter/HeaderAuthenticationFilter.java
+++ main/src/main/java/com/service/main/filter/HeaderAuthenticationFilter.java
@@ -46,7 +46,8 @@ public class HeaderAuthenticationFilter extends OncePerRequestFilter {
         if (userId == null || userRole == null) {
             res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             res.setContentType("application/json");
-            res.getWriter().write("{\"message\":\"Request is not authenticated\",\"error\":\"UNAUTHORIZED\"}");       
+            res.getWriter().write("{\"message\":\"Request is not authenticated\",\"error\":\"UNAUTHORIZED\"}");
+            return;
         }
 
         UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
